0ef4cdd504ccffca4d562c00fc643edd50872dfd
krigora <ihor.kryvynskyy@icloud.com>
2022-02-02 21:18:53 -0600

    create pojo class (@Data - lombok. data - automatic create getter and setter)

---
+++ src/test/java/com/cybertek/tests/pojo/SpartanJsonToPojoTest.java
@@ -0,0 +1,26 @@
+package com.cybertek.tests.pojo;
+
+import com.cybertek.tests.SpartanTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class SpartanJsonToPojoTest extends SpartanTestBase {
+
+    @Test
+    public void singleSpartanToMapTest() {
+        Response response = given().accept(ContentType.JSON)
+                .when().get("/api/spartans/24");
+
+        Spartan spartan = response.as(Spartan.class);
+        System.out.println(spartan.getId());
+        System.out.println(spartan.getName());
+        System.out.println(spartan.getGender());
+        System.out.println(spartan.getPhone());
+
+
+
+    }
+}
